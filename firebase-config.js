42671f6fbdfc84cbcd14883bd4daf6aeb2767387
Chester Mikhail De Guzman <140163654+chesterdeguzman@users.noreply.github.com>
2026-08-18 16:28:07 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD05cVCa_mGNoc3pqiOOkO7U7nkLlGmhGs",
+  authDomain: "studio5-content-calendar.firebaseapp.com",
+  databaseURL: "https://studio5-content-calendar-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "studio5-content-calendar",
+  storageBucket: "studio5-content-calendar.firebasestorage.app",
+  messagingSenderId: "846387526758",
+  appId: "1:846387526758:web:a53d080543cd6f3ca5c1a7"
+};
